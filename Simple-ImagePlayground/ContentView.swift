5d963a1b3eafd8dd840a75feb75576047f30eb0d
Nonprawich I. <40120130+npwitk@users.noreply.github.com>
2024-11-19 14:27:19 +0700

    Modified and Add Photos

--- Simple-ImagePlayground/ContentView.swift
+++ Simple-ImagePlayground/ContentView.swift
@@ -14,11 +14,15 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            
             if let imageURL {
                 AsyncImage(url: imageURL, scale: 1, content: { image in
                     image.resizable().aspectRatio(contentMode: .fit).frame(maxHeight: 300)
-                }, placeholder: { EmptyView() })
+                        .cornerRadius(10)
+                }, placeholder: { ContentUnavailableView("No Image Generated Yet", systemImage: "apple.image.playground.fill") })
                 .frame(height: 300)
+            } else {
+                ContentUnavailableView("No Image Generated Yet", systemImage: "apple.image.playground.fill")
             }
             Button {
                 showImagePlayground.toggle()
@@ -31,6 +35,7 @@ struct ContentView: View {
         .imagePlaygroundSheet(isPresented: $showImagePlayground, concept: "", onCompletion: {
             self.imageURL = $0
         })
+        .presentationSizing(.page)
     }
 }
 
